14b9d4ebe92c5b4a6a8a9055884b5fdb70d9c7bf
sayf4all <sayf4all@hotmail.com>
2019-05-28 15:12:58 +0200

    Voegt default aantal = 1

--- src/main/java/nl/multicode/bsngenerator/App.java
+++ src/main/java/nl/multicode/bsngenerator/App.java
@@ -5,7 +5,11 @@ import java.util.Set;
 
 public class App {
     public static void main(String[] args) {
-        Integer aantal = Integer.valueOf(args[0]);
+        Integer aantal = 1;
+        if(args.length > 0){
+            aantal = Integer.valueOf(args[0]);
+        }
+       
         BsnGenerator bsnGenerator = new BsnGenerator();
 
         Set<String> bsnSet = bsnGenerator.generateRandomBsnNummers(aantal);
