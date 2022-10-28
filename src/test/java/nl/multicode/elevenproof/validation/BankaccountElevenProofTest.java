1588c5a25ff8b0786fcd0ac97d67be9f2eca4adc
sayfjawad <sayf@multicode.nl>
2022-10-29 00:44:14 +0200

    Better unit test naming

--- src/test/java/nl/multicode/elevenproof/validation/BankaccountElevenProofTest.java
+++ src/test/java/nl/multicode/elevenproof/validation/BankaccountElevenProofTest.java
@@ -3,30 +3,38 @@ package nl.multicode.elevenproof.validation;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class BankaccountElevenProofTest {
 
-  private BankAccountElevenProof bankaccountElevenProof;
+
+  private BankAccountElevenProof bankAccountElevenProof;
 
   @BeforeEach
   public void setup() {
 
-    bankaccountElevenProof = new BankAccountElevenProof();
+    bankAccountElevenProof = new BankAccountElevenProof();
   }
 
-  @Test
-  void isGeldigBSN_True() {
+  @ParameterizedTest
+  @CsvSource({"0609567128", "0152180192", "0156575477", "0154858838"})
+  @DisplayName("Given that the isElevenProof is doing the BankAccount proof correctly "
+      + "When the method is called with a valid account number"
+      + "Then TRUE is returned")
+  void isElevenProof_True(String number) {
 
-    assertThat(bankaccountElevenProof.isElevenProof("0248199544")).isTrue();
+    assertThat(bankAccountElevenProof.isElevenProof(number)).isTrue();
   }
 
-  @Test
-  void isGeldigBSN_False() {
+  @ParameterizedTest
+  @CsvSource({"1112223301", "0177177771", "2212220231", "1209786511"})
+  @DisplayName("Given that the isElevenProof is doing the BankAccount proof correctly "
+      + "When the method is called with an invalid BankAccount number"
+      + "Then FALSE is returned")
+  void isElevenProof_False(String number) {
 
-    assertThat(bankaccountElevenProof.isElevenProof("0123456788")).isFalse();
-    assertThat(bankaccountElevenProof.isElevenProof("2108123325")).isFalse();
-    assertThat(bankaccountElevenProof.isElevenProof("111111111")).isFalse();
-    assertThat(bankaccountElevenProof.isElevenProof("X11111111111")).isFalse();
+    assertThat(bankAccountElevenProof.isElevenProof(number)).isFalse();
   }
 }
