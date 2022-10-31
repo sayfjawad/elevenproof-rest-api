79423cdade48b6ec51ca457d56868466cef49d2a
sayf <sayf.jawad@alliander.com>
2022-10-31 17:05:19 +0100

    Amending the unit tests with explanatory DisplayName

--- src/test/java/nl/multicode/elevenproof/generator/BankAccountGeneratorTest.java
+++ src/test/java/nl/multicode/elevenproof/generator/BankAccountGeneratorTest.java
@@ -3,19 +3,26 @@ package nl.multicode.elevenproof.generator;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import nl.multicode.elevenproof.model.ProofType;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class BankAccountGeneratorTest {
 
-  @Test
-  void generate() {
+    @Test
+    @DisplayName("Given the generator is implemented correctly"
+        + "When the generate() method is called withe the correct ProofType"
+        + "Then a valid eleven-proof BankAccount number is generated and returned")
+    void generate() {
 
-    assertThat(new BankAccountGenerator().generate(ProofType.BANK_ACCOUNT)).isNotEmpty();
-  }
+        assertThat(new BankAccountGenerator().generate(ProofType.BANK_ACCOUNT)).isNotEmpty();
+    }
 
-  @Test
-  void generate_empty() {
+    @Test
+    @DisplayName("Given the generator is implemented correctly"
+        + "When the generate() method is called with an invalid ProofType"
+        + "Then an empty Optional is returned")
+    void generate_empty() {
 
-    assertThat(new BankAccountGenerator().generate(ProofType.BSN)).isEmpty();
-  }
+        assertThat(new BankAccountGenerator().generate(ProofType.BSN)).isEmpty();
+    }
 }
--- src/test/java/nl/multicode/elevenproof/generator/BurgerServiceNummerGeneratorTest.java
+++ src/test/java/nl/multicode/elevenproof/generator/BurgerServiceNummerGeneratorTest.java
@@ -3,19 +3,27 @@ package nl.multicode.elevenproof.generator;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import nl.multicode.elevenproof.model.ProofType;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class BurgerServiceNummerGeneratorTest {
 
-  @Test
-  void generate() {
+    @Test
+    @DisplayName("Given the generator is implemented correctly"
+        + "When the generate() method is called withe the correct ProofType"
+        + "Then a valid eleven-proof BSN number is generated and returned")
+    void generate() {
 
-    assertThat(new BurgerServiceNummerGenerator().generate(ProofType.BSN)).isNotEmpty();
-  }
+        assertThat(new BurgerServiceNummerGenerator().generate(ProofType.BSN)).isNotEmpty();
+        assertThat(new BurgerServiceNummerGenerator().generate(ProofType.BSN).get()).hasSize(9);
+    }
 
-  @Test
-  void generate_empty() {
+    @Test
+    @DisplayName("Given the generator is implemented correctly"
+        + "When the generate() method is called with an invalid ProofType"
+        + "Then an empty Optional is returned")
+    void generate_empty() {
 
-    assertThat(new BurgerServiceNummerGenerator().generate(ProofType.BANK_ACCOUNT)).isEmpty();
-  }
+        assertThat(new BurgerServiceNummerGenerator().generate(ProofType.BANK_ACCOUNT)).isEmpty();
+    }
 }
