46e26cf9956a998695af6ac9612cfbd2f56b3e98
sayf <sayf.jawad@alliander.com>
2022-11-01 10:58:32 +0100

    Adding DisplayName to unit tests

--- src/test/java/nl/multicode/elevenproof/service/UnknownElevenProofServiceTest.java
+++ src/test/java/nl/multicode/elevenproof/service/UnknownElevenProofServiceTest.java
@@ -3,27 +3,34 @@ package nl.multicode.elevenproof.service;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class UnknownElevenProofServiceTest {
 
-  private UnknownElevenProofService service;
+    private UnknownElevenProofService service;
 
-  @BeforeEach
-  public void before() {
+    @BeforeEach
+    public void before() {
 
-    service = new UnknownElevenProofService();
-  }
+        service = new UnknownElevenProofService();
+    }
 
-  @Test
-  void generate() {
+    @Test
+    @DisplayName("Given the Service is working correctly"
+        + "When the generate() method is called"
+        + "Then an empty optional is returned ")
+    void generate() {
 
-    assertThat(service.generate()).isEmpty();
-  }
+        assertThat(service.generate()).isEmpty();
+    }
 
-  @Test
-  void isValid() {
+    @Test
+    @DisplayName("Given the Service is working correctly"
+        + "When the isValid() method is called"
+        + "Then a false boolean is returned ")
+    void isValid() {
 
-    assertThat(service.isValid("anyNumber")).isFalse();
-  }
+        assertThat(service.isValid("anyNumber")).isFalse();
+    }
 }
