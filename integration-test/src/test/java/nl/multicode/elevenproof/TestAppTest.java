a5e73b75e4c73a6c186de593cf7ffc46154a6e43
sayfjawad <sayf@multicode.nl>
2023-04-09 12:19:40 +0200

    WIP adding module for integration testing

---
+++ integration-test/src/test/java/nl/multicode/elevenproof/TestAppTest.java
@@ -0,0 +1,13 @@
+package nl.multicode.elevenproof;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestAppTest {
+
+  @Test
+  void main() {
+
+  }
+}
