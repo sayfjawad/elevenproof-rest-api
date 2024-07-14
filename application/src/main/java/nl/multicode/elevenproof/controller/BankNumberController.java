44d80d6dc0897e535863ea74be5fd1ce1b8e38aa
sayf <sayf@multicode.nl>
2024-07-14 21:58:22 +0200

    WIP gradle

--- application/src/main/java/nl/multicode/elevenproof/controller/BankNumberController.java
+++ application/src/main/java/nl/multicode/elevenproof/controller/BankNumberController.java
@@ -1,38 +1,38 @@
-//package nl.multicode.elevenproof.controller;
-//
-//import lombok.RequiredArgsConstructor;
-//import nl.multicode.elevenproof.model.BankAccountNumberDto;
-//import nl.multicode.elevenproof.openapi.model.BankAccountNumber;
-//import nl.multicode.elevenproof.service.ElevenProofService;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//
-//@RequestMapping("/api/bank")
-//@RestController
-//@RequiredArgsConstructor
-//public class BankNumberController implements
-//        ElevenproofController<ResponseEntity<BankAccountNumber>> {
-//
-//    private final ElevenProofService<BankAccountNumberDto> service;
-//
-//    @GetMapping("/generate")
-//    public ResponseEntity<BankAccountNumber> generate() {
-//
-//        return ResponseEntity.ok(BankAccountNumber.builder()
-//                .number(service.generate().number())
-//                .build());
-//    }
-//
-//    @GetMapping("/validate/{number}")
-//    public ResponseEntity<BankAccountNumber> validate(@PathVariable("number") String number) {
-//
-//        return ResponseEntity.ok(BankAccountNumber.builder()
-//                .number(number)
-//                .isElevenproof(service.isValid(number))
-//                .build());
-//    }
-//}
+package nl.multicode.elevenproof.controller;
+
+import lombok.RequiredArgsConstructor;
+import nl.multicode.elevenproof.model.BankAccountNumberDto;
+import nl.multicode.elevenproof.openapi.model.BankAccountNumber;
+import nl.multicode.elevenproof.service.ElevenProofService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RequestMapping("/api/bank")
+@RestController
+@RequiredArgsConstructor
+public class BankNumberController implements
+        ElevenproofController<ResponseEntity<BankAccountNumber>> {
+
+    private final ElevenProofService<BankAccountNumberDto> service;
+
+    @GetMapping("/generate")
+    public ResponseEntity<BankAccountNumber> generate() {
+
+        final var accountNumber = new BankAccountNumber();
+        accountNumber.setNumber(service.generate().number());
+        return ResponseEntity.ok(accountNumber);
+    }
+
+    @GetMapping("/validate/{number}")
+    public ResponseEntity<BankAccountNumber> validate(@PathVariable("number") String number) {
+
+        final var accountNumber = new BankAccountNumber();
+        accountNumber.setNumber(number);
+        accountNumber.setIsElevenproof(service.isValid(number));
+        return ResponseEntity.ok(accountNumber);
+    }
+}
