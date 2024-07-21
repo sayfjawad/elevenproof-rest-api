0b26986871932ac83db61118aa3a3c5c50ac730c
sayf <sayf@multicode.nl>
2024-07-21 12:29:25 +0200

    WIP working setup in gradle withouth CICD

--- application/src/main/java/nl/multicode/elevenproof/config/AppConfig.java
+++ application/src/main/java/nl/multicode/elevenproof/config/AppConfig.java
@@ -1,5 +1,7 @@
 package nl.multicode.elevenproof.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 import nl.multicode.elevenproof.generate.BankAccountNumberGenerator;
 import nl.multicode.elevenproof.generate.BurgerServiceNummerGenerator;
 import nl.multicode.elevenproof.generate.supplier.FixedLengthStringRandomNumbersSupplier;
@@ -14,6 +16,16 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+
+        final var openapi = new OpenAPI();
+        openapi.addServersItem(new Server().url("https://elevenproof.multicode.nl"));
+        openapi.addServersItem(new Server().url("http://elevenproof.multicode.nl"));
+        openapi.addServersItem(new Server().url("http://localhost:8080"));
+        return openapi;
+    }
 
     @Bean
     public BankAccountNumberGenerator getBankAccountNumberGenerator(
