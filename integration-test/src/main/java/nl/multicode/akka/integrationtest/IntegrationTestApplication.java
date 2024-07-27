10d0f1f839057ea945f31fa8b5b05481cfcfbe42
sayf <sayf@multicode.nl>
2024-07-27 14:39:25 +0200

    WIP adding missing integration test

---
+++ integration-test/src/main/java/nl/multicode/akka/integrationtest/IntegrationTestApplication.java
@@ -0,0 +1,14 @@
+package nl.multicode.akka.integrationtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationTestApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(IntegrationTestApplication.class, args);
+    }
+
+}
---
+++ integration-test/src/test/java/nl/multicode/akka/integrationtest/IntegrationTestApplicationTests.java
@@ -0,0 +1,14 @@
+package nl.multicode.akka.integrationtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
